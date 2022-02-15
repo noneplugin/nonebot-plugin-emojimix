b47a553be42f8de0dafa3014e3a91b42fa02a13f
meetwq <meetwq@gmail.com>
2022-02-15 16:17:35 +0800

    fix config

--- nonebot_plugin_emojimix/__init__.py
+++ nonebot_plugin_emojimix/__init__.py
@@ -4,23 +4,24 @@ from nonebot.adapters.onebot.v11 import MessageSegment
 
 from .data_source import mix_emoji
 
-pattern = '[\u200d-\U0001fab5]'
-emojimix = on_regex(rf'^(?P<code1>{pattern})\s*\+\s*(?P<code2>{pattern})$',
-                    block=True, priority=13)
+pattern = "[\u200d-\U0001fab5]"
+emojimix = on_regex(
+    rf"^(?P<code1>{pattern})\s*\+\s*(?P<code2>{pattern})$", block=True, priority=13
+)
 
 
-__help__plugin_name__ = 'emojimix'
-__des__ = 'emoji合成器'
-__cmd__ = '{emoji1}+{emoji2}'
+__help__plugin_name__ = "emojimix"
+__des__ = "emoji合成器"
+__cmd__ = "{emoji1}+{emoji2}"
 __short_cmd__ = __cmd__
-__example__ = '😎+😁'
-__usage__ = f'{__des__}\nUsage:\n{__cmd__}\nExample:\n{__example__}'
+__example__ = "😎+😁"
+__usage__ = f"{__des__}\nUsage:\n{__cmd__}\nExample:\n{__example__}"
 
 
 @emojimix.handle()
 async def _(msg: dict = RegexDict()):
-    emoji_code1 = msg['code1']
-    emoji_code2 = msg['code2']
+    emoji_code1 = msg["code1"]
+    emoji_code2 = msg["code2"]
     result = await mix_emoji(emoji_code1, emoji_code2)
     if isinstance(result, str):
         await emojimix.finish(result)
--- nonebot_plugin_emojimix/data_source.py
+++ nonebot_plugin_emojimix/data_source.py
@@ -5,25 +5,20 @@ from nonebot import get_driver
 from nonebot.log import logger
 
 from .emoji import emojis, EmojiData
+from .config import Config
 
-global_config = get_driver().config
-proxy = global_config.http_proxy
-httpx_proxy = {
-    'http://': global_config.http_proxy,
-    'https://': global_config.http_proxy
-}
+emoji_config = Config.parse_obj(get_driver().config.dict())
 
-
-API = 'https://www.gstatic.com/android/keyboard/emojikitchen/'
+API = "https://www.gstatic.com/android/keyboard/emojikitchen/"
 
 
 def create_url(emoji1: EmojiData, emoji2: EmojiData) -> str:
     def emoji_code(emoji: EmojiData):
-        return '-'.join(map(lambda c: f'u{c:x}', emoji[0]))
+        return "-".join(map(lambda c: f"u{c:x}", emoji[0]))
 
     u1 = emoji_code(emoji1)
     u2 = emoji_code(emoji2)
-    return f'{API}{emoji1[2]}/{u1}/{u1}_{u2}.png'
+    return f"{API}{emoji1[2]}/{u1}/{u1}_{u2}.png"
 
 
 def find_emoji(emoji_code: str) -> Optional[EmojiData]:
@@ -38,23 +33,25 @@ async def mix_emoji(emoji_code1: str, emoji_code2: str) -> Union[str, bytes]:
     emoji1 = find_emoji(emoji_code1)
     emoji2 = find_emoji(emoji_code2)
     if not emoji1:
-        return f'不支持的emoji：{emoji_code1}'
+        return f"不支持的emoji：{emoji_code1}"
     if not emoji2:
-        return f'不支持的emoji：{emoji_code2}'
+        return f"不支持的emoji：{emoji_code2}"
 
     url1 = create_url(emoji1, emoji2)
     url2 = create_url(emoji2, emoji1)
     logger.info(url1)
     logger.info(url2)
     try:
-        async with httpx.AsyncClient(proxies=httpx_proxy, timeout=10) as client:
+        async with httpx.AsyncClient(
+            proxies=emoji_config.http_proxy, timeout=10
+        ) as client:
             resp = await client.get(url1)
             if resp.status_code == 200:
                 return resp.content
             resp = await client.get(url2)
             if resp.status_code == 200:
                 return resp.content
-            return '出错了，可能不支持该emoji组合'
+            return "出错了，可能不支持该emoji组合"
     except:
         logger.warning(traceback.format_exc())
-        return '下载出错，请稍后再试'
+        return "下载出错，请稍后再试"
